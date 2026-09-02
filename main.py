09c2c68e19b59ffd1e04cf5eaea965c929ef28d6
Agien99 <hypertyzer@gmail.com>
2026-09-02 12:19:38 +0800

    Test Dozens value

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from app.roulette import get_dozen
 
 print("=== Dozen Test ===")
 
-test_numbers = [0, 1, 12, 13, 24, 25, 36]
+test_numbers = [0, 1, 15, 19, 24, 33, 36]
 
 for number in test_numbers:
     print(f"{number} -> Dozen {get_dozen(number)}")
